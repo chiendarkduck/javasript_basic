2aee5cf669ddc248e557dc79398fb74b1698b4f3
chiendarkduck <nguyenhuuchien17112k2@gmail.com>
2023-07-06 14:44:58 +0700

    array ad object

---
+++ baitap/baitap.js
@@ -0,0 +1,31 @@
+
+// let a={
+//     name: 'chin',
+//     address: 'hanoi'
+// };
+// let b ='abc';
+// console.log('name: ' , a['name']);
+// console.log('address: ' ,a['address']);
+
+console.log(`hello world`);
+let a={
+    name: 'chien',
+    age: 21
+};
+console.log(`check data object:`, a);
+console.log(`my name's:`, a['name']);
+
+
+let b=['mu','chensi','liver'];
+console.log(`typee of b:`,typeof b, b);
+console.log('fisrt item:',b[0], 'second item:',b[1], 'third item:',b[2]);
+
+let family=[
+    {
+        name: 'chien',
+        age: 21},
+    {
+        name: 'quynh',
+        age: 19}
+]
+console.log(`my family:`,family);
