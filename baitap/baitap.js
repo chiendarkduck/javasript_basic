447560665518da2acad89feafb6389405bd1de23
chiendarkduck <nguyenhuuchien17112k2@gmail.com>
2023-07-07 16:11:35 +0700

    loops and condition

--- baitap/baitap.js
+++ baitap/baitap.js
@@ -7,25 +7,34 @@
 // console.log('name: ' , a['name']);
 // console.log('address: ' ,a['address']);
 
-console.log(`hello world`);
-let a={
-    name: 'chien',
-    age: 21
-};
-console.log(`check data object:`, a);
-console.log(`my name's:`, a['name']);
+// console.log(`hello world`);
+// let a={
+//     name: 'chien',
+//     age: 21
+// };
+// console.log(`check data object:`, a);
+// console.log(`my name's:`, a['name']);
 
 
-let b=['mu','chensi','liver'];
-console.log(`typee of b:`,typeof b, b);
-console.log('fisrt item:',b[0], 'second item:',b[1], 'third item:',b[2]);
+// let b=['mu','chensi','liver'];
+// console.log(`typee of b:`,typeof b, b);
+// console.log('fisrt item:',b[0], 'second item:',b[1], 'third item:',b[2]);
 
-let family=[
-    {
-        name: 'chien',
-        age: 21},
-    {
-        name: 'quynh',
-        age: 19}
-]
-console.log(`my family:`,family);
+// let family=[
+//     {
+//         name: 'chien',
+//         age: 21},
+//     {
+//         name: 'quynh',
+//         age: 19}
+// ]
+// console.log(`my family:`,family);
+
+console.log(`hello word right now`)
+let a=5;
+let b='5';
+console.log(`compare a == b:`,`a=`,a,`b=`,b,`result:`,a==b)
+console.log(`compare a === b:`,`a=`,a,`b=`,b,`result:`,a===b)
+
+//empty null undefined
+let c='';
